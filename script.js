7d233e55a7ab57d9136ebfa00d4bc020a8881060
Ron <mutungaron@gmail.com>
2026-07-27 19:12:26 +0300

    Added event listener to the form so that JS can listen to the form submission

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const form = document.getElementById("akanForm");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+});
