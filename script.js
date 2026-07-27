469a59c0e8b26b37e874cee082d6a8898babc3c2
Ron <mutungaron@gmail.com>
2026-07-27 20:08:11 +0300

    Added a result display and a name determinator

--- script.js
+++ script.js
@@ -1,59 +1,82 @@
 //array of male, female names and dates
-const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-const femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 //form submission
 const form = document.getElementById("akanForm");
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+  //Reading Inputs
+  const day = Number(document.getElementById("day").value);
+
+  const month = Number(document.getElementById("month").value);
+
+  const year = Number(document.getElementById("year").value);
+
+  const gender = document.querySelector('input[name="gender"]:checked');
+
+  //Validating inputs
+  if (
+    day < 1 ||
+    day > 31 ||
+    month < 1 ||
+    month > 12 ||
+    year <= 0 ||
+    gender === null
+  ) {
+    alert("Please put a valid date and select your gender.");
+
+    return;
+  }
+
+  //Splitting the year
+  const CC = Math.floor(year / 100);
+  const YY = year % 100;
+
+  //calculating the day
+  const dayNumber = Math.floor(
+
+    (
+      (4 * CC)
+      -
+      (2 * CC - 1)
+      +
+      ((5 * YY) / 4)
+      +
+      ((26 * (month + 1)) / 10)
+      +
+      day
+    )
+    % 7
+  );
+
+
+  //Describe the Akan Name
+  let akanName;
+
+  if (gender.value === "male") {
+    akanName = maleNames[dayNumber];
+  }
+
+  else {
+    akanName = femaleNames[dayNumber];
+  }
+
+
+  //Displaying the results
+  const output = document.getElementById("output");
+
+  output.innerHTML = `You were born on <strong> $[weekDays [dayNumber]]</strong>.
+<br><br>
+Your Akan name is 
+<br><br>
+<strong> ${akanName}</strong>`;
 });
 
-//Reading Inputs
-const day = Number(document.getElementById("day").value);
-
-const month = Number(document.getElementById("month").value);
-
-const year = Number(document.getElementById("year").value);
-
-const gender = document.querySelector('input[name="gender"]:checked');
-
-//Validating inputs
-if(
-  day < 1 ||
-  day > 31 ||
-  month < 1 ||
-  month > 12 || 
-  year <=0 || 
-  gender === null
-){
-  alert("Please put a valid date and select your gender.");
-
-  return;
-}
-
-//Splitting the year
-const CC = Math.floor(year / 100);
-const YY = year % 100;
-
-//calculating the day
-const dayNumber = Math.floor(
-
-  (
-    (4 * CC)
-    -
-    (2 * CC - 1)
-    +
-    ((5 * YY) / 4)
-    +
-    ((26 *(month + 1)) /10)
-    +
-    day
-  )
-  % 7
-);
 
 
