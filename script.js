1482e86c0d1dccc4f83345bf97f516982e731612
Ron <mutungaron@gmail.com>
2026-07-27 19:09:39 +0300

    Added arrays of names and dates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
-const femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama" ]
+const femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-cons
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
